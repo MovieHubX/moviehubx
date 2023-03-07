1a352db17571a490cf514ecb2f373e5ebc43f706
MovieHubX <126613066+MovieHubX@users.noreply.github.com>
2023-03-07 08:05:29 +0200

    Update Footer.tsx

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,5 +1,5 @@
-import { Helmet } from "react-helmet";
-import { FaCoffee } from "react-icons/fa";
+import { Helmet } from 'react-helmet';
+import { FaCoffee } from 'react-icons/fa';
 
 const Footer = () => {
   return (
